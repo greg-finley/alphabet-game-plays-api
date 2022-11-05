8b55c59201690ac2fb3f764084d526770808017f
Greg Finley <gregory.finley@gmail.com>
2022-11-05 16:08:25 -0700

    Sport

--- main.py
+++ main.py
@@ -7,6 +7,18 @@ def main(request):
     limit = request.args.get("limit", 50)
     limit = min(1000, int(limit))
 
+    sport = request.args.get("sport")
+    if sport and sport not in ["NBA", "MLB", "NFL", "NHL"]:
+        return (
+            {"error": "Invalid sport. Options are: NHL, NBA, MLB, and NFL."},
+            400,
+            {"Content-Type": "application/json"},
+        )
+    if sport:
+        sport = f" AND sport = '{sport}'"
+    else:
+        sport = ""
+
     client = bigquery.Client()
     query = f"""
                 SELECT * except
@@ -15,7 +27,7 @@ def main(request):
                 case when tweet_text like '%is still%' then false else true
                 end as letter_match
                 FROM mlb_alphabet_game.tweetable_plays
-                where deleted = false
+                where deleted = false {sport}
                 order by completed_at desc limit {limit}
             """
     results = client.query(query).result()
